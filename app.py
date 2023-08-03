54587e0b7a0fad12fbbf7967ba8a9a9844a255df
hennastone <ahmetfrknknts@gmail.com>
2023-08-03 12:00:14 +0300

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from holistic import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hey"
+    main()
+    return "Mete Gazoz"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
